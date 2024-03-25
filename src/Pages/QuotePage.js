510ef6b9fac7c7563b8dc71b4824c3afbe16e12a
Jake-W95 <jakewallace368@gmail.com>
2024-03-25 10:37:08 +0000

    improves css

--- src/Pages/QuotePage.js
+++ src/Pages/QuotePage.js
@@ -1,20 +1,19 @@
 
 
 
- const QuotePage = ({int}) => {
+const QuotePage = ({ int }) => {
 
-const reviews = [['Anna H', '"I would higly \n recommend Billy to anyone looking for a well built bike, cared for with intention"'], ['Stanley J', '"I was really pleased with the service Billy provided. I\'ve used lots of other mechanics before and I\'ve rarely had such quality service, and for a really reasonable price... \n I will always be making the trip to Billy\'s from now on and would recommend Billy\'s Bike Exchange to anyone!"'], ['Steve M', '"Really friendly guy, well priced and clearly really passionate about his work... \n100% recommend"']]
+    const reviews = [['Anna H', '"I would higly \n recommend Billy to anyone looking for a well built bike, cared for with intention"'], ['Stanley J', '"I was really pleased with the service Billy provided. I\'ve used lots of other mechanics before and I\'ve rarely had such quality service, and for a really reasonable price... \n I will always be making the trip to Billy\'s from now on and would recommend Billy\'s Bike Exchange to anyone!"'], ['Steve M', '"Really friendly guy, well priced and clearly really passionate about his work... \n100% recommend"']]
 
-const reviewer = reviews[int][0]
-const review1 = reviews[int][1]
-console.log(review1)
+    const reviewer = reviews[int][0]
+    const review1 = reviews[int][1]
     return (
         <>
-        <section className='page quotePage' >
-            <p className="kalam-bold">{reviewer}</p>
-            <p className="kalam-regular">{review1}</p>
+            <section className='page quotePage' >
+                <p className="kalam-bold fa3">{reviewer}</p>
+                <p className="kalam-regular fa2" style={{margin: '.5em'}}>{review1}</p>
 
-        </section>
+            </section>
         </>
     )
 
